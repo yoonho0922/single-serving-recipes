73a247add713dc00acce06a710eef0faafa56dd7
yoonho0922 <yoonho5684@gmail.com>
2024-07-16 15:11:30 +0900

    init

---
+++ src/main/java/com/yoon/foundation/javaspringfoundation/JavaSpringFoundationApplication.java
@@ -0,0 +1,12 @@
+package com.yoon.foundation.javaspringfoundation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringFoundationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringFoundationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yoon/foundation/javaspringfoundation/JavaSpringFoundationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoon.foundation.javaspringfoundation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringFoundationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
