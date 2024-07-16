b733ba6a23fd1eca206bf2045a777785ef92bf77
yoonho0922 <yoonho5684@gmail.com>
2024-07-17 00:37:38 +0900

    fix: update index controller

--- src/main/java/com/yoon/foundation/controller/IndexController.java
+++ src/main/java/com/yoon/foundation/controller/IndexController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
     @GetMapping
     public String index() {
-        return "Hello, World!";
+        return "Hello, World! with CI/CD";
     }
 }
