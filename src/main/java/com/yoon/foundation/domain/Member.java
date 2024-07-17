2124089d16f3867a81d118d0da5b82a4006b2d80
yoonho0922 <yoonho5684@gmail.com>
2024-07-17 09:51:53 +0900

    feat: RDS Maria DB 연결

---
+++ src/main/java/com/yoon/foundation/domain/Member.java
@@ -0,0 +1,11 @@
+package com.yoon.foundation.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+}
